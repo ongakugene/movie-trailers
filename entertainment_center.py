9fe2b459cdf1ec7647be140857bc69bd28e30122
Nikhil Khullar <nikhil.khullar786@gmail.com>
2015-07-17 10:27:37 +0530

    Initial Commit

---
+++ entertainment_center.py
@@ -0,0 +1,7 @@
+__author__ = 'ali786'
+
+import media
+
+# Creating instances of Movie class and these movies will show up on the web page
+virgin_snow = media.Movie("Virgin Snow", "Returning to Japan, a man tries to find the beautiful woman who captured his heart.", "https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Virgin_Snow_%28film%29.jpg/220px-Virgin_Snow_%28film%29.jpg", "https://www.youtube.com/watch?v=P2mRL3ZBfp0")
+paradise_kiss = media.Movie("Paradise Kiss", "A heartwarming love story.", "https://upload.wikimedia.org/wikipedia/en/thumb/0/08/Paradise_Kiss_Movie_Poster.jpg/220px-Paradise_Kiss_Movie_Poster.jpg", "https://www.youtube.com/watch?v=zkH_4nDmOTM")
---
+++ media.py
@@ -0,0 +1,8 @@
+__author__ = 'ali786'
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+        self.title = movie_title
+        self.story_line = movie_storyline
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
