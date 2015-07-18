8259e9cade36583bcd34824066ddf176d3251af5
Nikhil Khullar <nikhil.khullar786@gmail.com>
2015-07-18 12:07:02 +0530

    Reformatted code to meet pep8 specification guidelines

--- media.py
+++ media.py
@@ -1,9 +1,15 @@
 __author__ = 'ali786'
 
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer, genre):
-        self.title = movie_title
-        self.story_line = movie_storyline
-        self.poster_image_url = movie_poster
-        self.trailer_youtube_url = movie_trailer
+    """
+    This class describes various attributes
+    for a movie.
+    """
+
+    def __init__(self, title, storyline, poster, trailer, genre):
+        self.title = title
+        self.story_line = storyline
+        self.poster_image_url = poster
+        self.trailer_youtube_url = trailer
         self.genre = genre
